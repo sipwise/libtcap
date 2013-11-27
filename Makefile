df33deecd174c1be6105ca573845aa48fead0a59
Richard Fuchs <rfuchs@sipwise.com>
2013-11-27 14:38:19 -0500

    adding support files and stub files

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
-# stub
-
-
-.PHONY: all regenerate-asn1
+.PHONY: all regenerate-asn1 clean
 
 all:
-	exit 1
+	$(MAKE) -C asn1-compiled
+	$(MAKE) -C src
 
 regenerate-asn1:
 	rm -f asn1-compiled/*
 	(cd asn1-compiled && asn1c -fskeletons-copy ../asn1/tcap.asn)
+	ln -s ../asn1/Makefile.compiled asn1-compiled/Makefile
+
+clean:
+	$(MAKE) -C asn1-compiled lib-clean
+	$(MAKE) -C src clean
---
+++ src/tcap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include "TCMessage.h"
+
+
+
+
+TCMessage_t *tcap_decode(const char *buf, size_t len) {
+	TCMessage_t *ret = NULL;
+	asn_dec_rval_t rv;
+
+	rv = ber_decode(0, &asn_DEF_TCMessage, (void **) &ret, buf, len);
+
+	if (rv.code == RC_OK)
+		return ret;
+
+	asn_DEF_TCMessage.free_struct(&asn_DEF_TCMessage, ret, 0);
+	return NULL;
+}
